2d4a8829871f04e9c4d85aa0d22d094e7dae38f1
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-11-10 18:12:32 +0000

    Added user model

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
   "net/http"
 )
 
+type person struct {
+  Name     string    `json="name"`
+  Birthday time.Time `json="birthday"`
+  Email    string    `json="email"`
+}
+
+func (p *person) Age() int {
+  return 1
+}
+
 func main() {
 
   http.HandleFunc("/", Hello)
