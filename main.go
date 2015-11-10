6aea147f1aa597fbb0c7d01fde2cc324df6f5445
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-11-10 19:02:17 +0000

    Returning json

--- main.go
+++ main.go
@@ -5,22 +5,23 @@ import (
   "log"
   "time"
   "net/http"
+  "encoding/json"
 )
 
-type person struct {
-  Name     string    `json="name"`
-  Birthday time.Time `json="birthday"`
-  Email    string    `json="email"`
+type Person struct {
+  Name  string    `json="name"`
+  Birth time.Time `json="birth"`
+  Email string    `json="email"`
 }
 
-func (p *person) Age() int {
+func (p *Person) Age() int {
   return 1
 }
 
 func main() {
 
   http.HandleFunc("/", Hello)
-
+  http.HandleFunc("/user", GetUser)
 
   s := &http.Server{
     Addr:           ":8080",
@@ -32,7 +33,21 @@ func main() {
   log.Fatal(s.ListenAndServe())
 }
 
-
+// Hello
 func Hello (w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "Hello World")
+}
+
+// GetUser
+func GetUser(w http.ResponseWriter, r *http.Request) {
+  
+  user, err := json.Marshal(Person{ Name:"Thiago Augustus de Oliveira", Birth: time.Date(1983, time.June, 9, 23, 0, 0, 0, time.UTC), Email:"thiagoaugustusdeoliveira@gmail.com"})
+  
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w, "%s", user)
 }
